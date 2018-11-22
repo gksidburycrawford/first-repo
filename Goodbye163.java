880b24643be5e3c74be550f93921862c2968a22b
Geterry Sidbury Crawford <gksidburycrawford@aggies.ncat.edu>
2018-11-21 23:41:41 -0500

    commit first program

---
+++ Goodbye163.java
@@ -0,0 +1,13 @@
+/**
+ * This class prints out a statement
+ * @gksidburycrawford@aggies.ncat.edu
+**/
+public class Goodbye163 {
+   /**
+    * This main method prints out a farewell message,
+    * "Goodbye COMP 163"
+    **/
+   public static void main(String[] args) {
+      System.out.println("Goodbye COMP 163");
+   }
+}
